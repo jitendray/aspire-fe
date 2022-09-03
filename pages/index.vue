7a989683a56edbc1b51338616a53b74ae3b6d077
Jitendra Yadav <jeetu.yadav28@gmail.com>
2022-09-04 01:47:44 +0530

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,65 @@
 <template>
-  <Tutorial/>
+  <!-- <Tutorial/> -->
+
+  <!-- Card Design [Start] -->
+  <!-- component -->
+  <div class="flex justify-center items-center">
+    <div class="space-y-16">
+      <div
+        class="
+          w-96
+          h-56
+          m-auto
+          bg-green-400
+          rounded-xl
+          relative
+          text-white
+          shadow-md
+          transition-transform
+          transform
+        "
+      >
+
+        <div class="w-full px-8 absolute top-8">
+          <div class="flex justify-end">
+             <img
+              class="h-21 w-74 text-white"
+              src="/Logo-2.svg"
+            />
+          </div>
+          <div class="flex justify-between">
+              <p class="font-sans font-bold tracking-wide text-xl leading-5">Mark Henry</p>
+          </div>
+          <div class="pt-1">
+            <p class="font-medium tracking-more-wider font-mono">
+                ●●●● ●●●● ●●●● 9867
+            </p>
+          </div>
+          <div class="pt-6 pr-6">
+            <div class="flex justify-between">
+              <div class="">
+                <p class="font-medium tracking-wider text-sm font-mono">
+                  03/25
+                </p>
+              </div>
+
+              <div class="">
+                <p class="font-light text-xs">CVC</p>
+                <p class="font-bold tracking-more-wider text-sm font-mono">
+                  123
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Card Design [End] -->
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
